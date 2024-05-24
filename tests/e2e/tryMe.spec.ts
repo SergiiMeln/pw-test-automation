52f6ce83460366c2fa0bb77222b679cf0e0ba918
SergiiMeln <53070257+SergiiMeln@users.noreply.github.com>
2024-05-24 00:23:31 -0400

    Feedback Form Test Suit (#2)
    * Add Smoke and Regression tags
    
    * Add Feedback Form Tests
    
    ---------
    
    Co-authored-by: Sergii Meln <sergiimeln@gmail.com>

--- tests/e2e/tryMe.spec.ts
+++ tests/e2e/tryMe.spec.ts
@@ -1,8 +1,8 @@
 import {test, expect} from "@playwright/test"
 
-test.describe("TryMe test suit", ()=> {
+test.describe("TryMe Test Suit", ()=> {
     
-    test("First", async ({page}) => {
+    test("First", {tag:"@Smoke"}, async ({page}) => {
         console.log("Fist test runs");
     })
 
@@ -12,5 +12,13 @@ test.describe("TryMe test suit", ()=> {
 
     test("Third", async ({page}) => {
         console.log("Third test runs");
+    })
+
+    test("Forth", async ({page}) => {
+        console.log("Forth test runs");
+    })
+
+    test("Fifth", async ({page}) => {
+        console.log("Fifth test runs");
     })
 })
