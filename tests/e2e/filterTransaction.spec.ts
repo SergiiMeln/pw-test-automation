ce09d9ffa4b51c17c69db1a7fa3f2bdef80eea50
SergiiMeln <53070257+SergiiMeln@users.noreply.github.com>
2024-06-04 00:02:54 -0400

    Acctransactions (#12)
    * Add account activity test
    
    ---------
    
    Co-authored-by: Sergii Meln <sergiimeln@gmail.com>

--- tests/e2e/filterTransaction.spec.ts
+++ tests/e2e/filterTransaction.spec.ts
@@ -1,4 +1,6 @@
-import {test, expect} from "@playwright/test"
+import {test, expect, Locator} from "@playwright/test"
+        let accountList: Locator;
+        let pageTitle: string | null;
 
 test.describe.only("Filter Account Transactions", ()=> {
     test.beforeEach(async ({page}) => {
@@ -9,11 +11,34 @@ test.describe.only("Filter Account Transactions", ()=> {
         await page.getByText("Sign in").click()
         await page.goto("http://zero.webappsecurity.com", {waitUntil:"domcontentloaded"})
         await page.click("#account_activity_link");
-        await page.waitForTimeout(2000);
+        pageTitle = await page.locator(".board-header").textContent();
+        expect(pageTitle).toContain("Show Transactions");
+        accountList = page.locator("#aa_accountId");
     })
 
 
-    test("Check Savings Activities", async({page})=> {
-        console.log("Account Savings");
+    test("Verify Checking Account Activities", async({page})=> {
+        //await page.selectOption("#aa_accountId", "2");
+        await accountList.selectOption("2")
+        await expect(accountList).toContainText("Checking");
+//assert number of reconds
+        const accountActivities = page.locator("#all_transactions_for_account table tbody>tr");
+        await expect(accountActivities).toHaveCount(3);
+
+//assert columns titles
+        const accountColumn = await page.locator("#all_transactions_for_account table thead>tr>th").allTextContents();
+        //console.log(accountColumn);
+        await expect(page.locator("#all_transactions_for_account table thead>tr>th")).toHaveText(['Date', 'Description', 'Deposit', 'Withdrawal']);
+
+//assert first row in table
+        const accountRecords = await page.locator("#all_transactions_for_account table tbody>tr:first-child>td").allTextContents()
+        //console.log(accountRecords);
+        await expect(page.locator("#all_transactions_for_account table tbody>tr:first-child>td")).toHaveText([ '2012-09-06', 'CHECK DEPOSIT', '186.7', '' ]);
+ 
+//prints account names
+    const all = await page.locator("#aa_accountId>option").all()
+    for(let i=0; i<all.length; i++){
+        console.log(await all[i].textContent());
+    }
     })
 })
